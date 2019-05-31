ba512d42d926268e178e00df8a6977807eb9e1ac
Lillian <48282078+Lily0727K@users.noreply.github.com>
2019-05-31 17:30:58 +0900

    数独ソルバー

--- sudoku.py
+++ sudoku.py
@@ -1,38 +1,54 @@
-def rec(field):
-    emptyi, emptyj = -1, -1
-    for i in range(9):
-        for j in range(9):
-            if field[i][j] == 0:
-                emptyi, emptyj = i, j
+def solve(puzzle):
 
-    if emptyi == -1 and emptyj == -1:
+    def print_field(field):
         for row in field:
             print(*row, sep=" ")
-        exit()
 
-    canuse = [True] * 10
+    def find_empty(field):
+        emptyi, emptyj = -1, -1
+        for i in range(9):
+            for j in range(9):
+                if field[i][j] == 0:
+                    emptyi, emptyj = i, j
+        return emptyi, emptyj
 
-    for i in range(9):
-        if field[emptyi][i] != 0:
-            canuse[field[emptyi][i]] = False
-        if field[i][emptyj] != 0:
-            canuse[field[i][emptyj]] = False
+    def find_possible_num(field, emptyi, emptyj):
+        canuse = [True] * 10
+        for i in range(9):
+            if field[emptyi][i] != 0:
+                canuse[field[emptyi][i]] = False
+            if field[i][emptyj] != 0:
+                canuse[field[i][emptyj]] = False
 
-        bi, bj = emptyi // 3 * 3 + 1, emptyj // 3 * 3 + 1
+            bi, bj = emptyi // 3 * 3 + 1, emptyj // 3 * 3 + 1
 
-        for di in range(bi-1, bi+2):
-            for dj in range(bj-1, bj+2):
-                canuse[field[di][dj]] = False
+            for di in range(bi-1, bi+2):
+                for dj in range(bj-1, bj+2):
+                    canuse[field[di][dj]] = False
+        return canuse
 
-    for i in range(1, 10):
-        if canuse[i]:
-            field[emptyi][emptyj] = i
-            rec(field)
+    def rec(field):
+        emptyi, emptyj = find_empty(field)
 
-    field[emptyi][emptyj] = 0
+        if emptyi == -1 and emptyj == -1:
+            global answer
+            answer = [" ".join([str(x) for x in row]) for row in field]
+            print_field(field)
+            return
+
+        canuse = find_possible_num(field, emptyi, emptyj)
+
+        for i in range(1, 10):
+            if canuse[i]:
+                field[emptyi][emptyj] = i
+                rec(field)
+
+        field[emptyi][emptyj] = 0
+
+    rec(puzzle)
 
 
-def main():
+if __name__ == "__main__":
     puzzle = [[0, 0, 6, 1, 0, 0, 0, 0, 8],
               [0, 8, 0, 0, 9, 0, 0, 3, 0],
               [2, 0, 0, 0, 0, 5, 4, 0, 0],
@@ -42,8 +58,4 @@ def main():
               [0, 0, 8, 4, 0, 0, 0, 0, 6],
               [0, 2, 0, 0, 5, 0, 0, 8, 0],
               [1, 0, 0, 0, 0, 2, 5, 0, 0]]
-    rec(puzzle)
-
-
-if __name__ == "__main__":
-    main()
+    solve(puzzle)
